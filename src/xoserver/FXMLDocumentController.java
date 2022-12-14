8c2e2902ba04a3a4326dc732e24960bb29208e37
mohamed-ie <mohammedie98@gmail.com>
2022-12-14 15:01:04 +0200

    first commit

--- src/xoserver/FXMLDocumentController.java
+++ src/xoserver/FXMLDocumentController.java
@@ -1,6 +1,7 @@
 /*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXML2.java to edit this template
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
  */
 package xoserver;
 
@@ -13,7 +14,7 @@ import javafx.scene.control.Label;
 
 /**
  *
- * @author mo_fathy
+ * @author mohamed
  */
 public class FXMLDocumentController implements Initializable {
     
