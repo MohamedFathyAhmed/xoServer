3805a59e8d94f965a76225d79e8af05477d42dfe
MohamedFathyAhmed <112295268+MohamedFathyAhmed@users.noreply.github.com>
2022-12-20 18:54:07 +0200

    fix_reqest Handler

--- src/data/database/DataAccessLayer.java
+++ src/data/database/DataAccessLayer.java
@@ -17,83 +17,25 @@ public class DataAccessLayer {
     private static Connection connection;
     private static ResultSet record;
 
-    public static String CreatePlayer(Player player) throws SQLException {
-        String res = "";
+public static boolean insertPlayer(String username, String password) {
         try {
-            PreparedStatement con = connection.prepareStatement("INSERT INTO Players (name , password , score) VALUES (?  , ? , ?)", ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_UPDATABLE);
-            con.setString(1, player.getName());
-            con.setString(2, player.getPassword());
-            con.setInt(3, player.getScore());
-            con.executeUpdate();
-            res = "true";
-
-        } catch (SQLException e) {
-            res = "false;;please try again";
-        } finally {
-            return res;
-        }
-    }
-
-        public boolean changeStatus(String name,Boolean status,Boolean IsPlaying) {
-        try {
-            PreparedStatement con = connection.prepareStatement("UPDATE players set status=?, IsPlaying=? WHERE NAME=?");
-            con.setBoolean(1,status );
-            con.setBoolean(2,IsPlaying );
-            con.setString(3,name );
-            int isupdated = con.executeUpdate();
-            if (isupdated > 0) {
-                return true;
-            }
-        } catch (SQLException e) {
-        }
-        return true;
- }
-        
-    public void changeScore(String name, int newScore) {
-        try {
-            PreparedStatement con = connection.prepareStatement("UPDATE players set score=score+? WHERE id=?");
-            con.setInt(1, newScore);
-            con.setString(3,name );
-
-            int updataNumber = con.executeUpdate();
-        } catch (SQLException e) {
-            System.out.println(e);
-        }
-    }
-
-    public static String getPlayer(String name, String password) {
-        String res = "";
-        try {
-            PreparedStatement con = connection.prepareStatement("SELECT * FROM players WHERE name=? AND password=?");
-            con.setString(1, name);
-            con.setString(2, password);
-            ResultSet rs = con.executeQuery();
-            boolean isExist = rs.next();
-
-            if (isExist) {
-                res = "true" + ";;" + rs.getString("name") + ";;" + rs.getInt("score");
-                PreparedStatement update = connection.prepareStatement("UPDATE players set status=TRUE WHERE name=? ");
-                update.setString(1, name);
-                int updataNumber = update.executeUpdate();
-                System.out.println(updataNumber);
-
-                Player player = new Player(
-                        rs.getString("name"),
-                        rs.getString("password"),
-                        rs.getInt("score"));
-
-            } else {
-                // if user don't exist
-                res = "false___notExist";
-            }
+            return !connection
+                    .createStatement()
+                    .execute("INSERT INTO PLAYER (NAME, PASSWORD) VALUES ('" + username + "','" + password + "')");
         } catch (SQLException ex) {
-            res = "false___error";
-            System.out.println(ex);
-        } finally {
-            return res;
+            return false;
         }
-
     }
 
+    public static boolean login(String username, String password) {
+        try {
+            return connection
+                    .createStatement()
+                    .executeQuery("SELECT 1 FROM PLAYER WHERE NAME='" + username + "' AND PASSWORD='" + password + "'")
+                    .next();
+        } catch (SQLException ex) {
+            return false;
+        }
+    }
 
 }
--- src/xoserver/handlers/DataRequestHandler.java
+++ src/xoserver/handlers/DataRequestHandler.java
@@ -5,28 +5,129 @@
  */
 package xoserver.handlers;
 
+import data.database.DataAccessLayer;
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.List;
+import xoserver.handlers.client.ClientHandler;
+import xoserver.handlers.game_room.GameRoomHandler;
+
 /**
  *
- * @author mohamed
+ * @author fathy
  */
-public class DataRequestHandler {
+public class DataRequestHandler extends RequestHandler {
 
     private static DataRequestHandler instance;
 
     public DataRequestHandler() {
     }
 
-
     public ResponseReceiver newResponseReceiver() {
         return this::handle;
     }
 
-    private String handle(String request) {
-        switch (request) {
-            
-            
-            
+    private String handle(String name, String request) throws IOException, NullPointerException, SQLException {
+        String[] splitedRequest = request.split(RequestType.MESSAGE_SPLITER);
+        boolean success = false;
+        switch (splitedRequest[0]) {
+            case RequestType.ONLINE_PLAYERS:
+                return createOnlinePLayersResponse(name);
+            case RequestType.REQUEST_GAME:
+                success = sendToPlayer(createRequestGame(name), splitedRequest[1]);
+                return sendRequestGameResponse(success);
+            case RequestType.REQUEST_GAME_ANSWER:
+                success = Boolean.valueOf(splitedRequest[2]);
+                if (success) {
+                    sendToPlayer(createAnswerGameRsponse(success, splitedRequest[1]), splitedRequest[1]);
+                    getPlayerIntoGame(name, splitedRequest[1]);
+                }
+                return createAnswerGameRsponse(success, name);
+
+            case RequestType.HISTORY:
+                return createResponse(
+                        RequestType.HISTORY,
+                        "true",
+                        DataAccessLayer.getGames(name),
+                         "sent"
+                );
+
+            case RequestType.RECORDED_GAME:
+                return createResponse(
+                        RequestType.RECORDED_GAME,
+                        "true",
+                        DataAccessLayer.getGamePlays(Integer.parseInt(splitedRequest[1])),
+                        "sent"
+                );
+                
         }
         return "";
     }
+
+    private String createOnlinePLayersResponse(String name) {
+
+        return createResponse(
+                RequestType.ONLINE_PLAYERS,
+                "true",
+                "success",
+                getOnlinePlayersAsString(name));
+
+    }
+
+    private String getOnlinePlayersAsString(String name) {
+        List<ClientHandler> onlineClients = ClientHandler.getOnlineClientHandlers();
+        String clientsString = "";
+        String clientName;
+
+        for (ClientHandler client : onlineClients) {
+            clientName = client.getName();
+            if (!clientName.equals(name)) {
+                clientsString += clientName + RequestType.ARRAY_SPLITER;
+            }
+        }
+        return clientsString;
+    }
+
+    private boolean sendToPlayer(String response, String reciver) {
+        List<ClientHandler> onlineClients = ClientHandler.getOnlineClientHandlers();
+        String clientName;
+        for (ClientHandler client : onlineClients) {
+            clientName = client.getName();
+            if (clientName.equals(reciver)) {
+                try {
+                    client.request(response);
+                } catch (IOException ex) {
+                    return false;
+                }
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private String createRequestGame(String sender) {
+        return createResponse(RequestType.REQUEST_GAME, "true", sender + " requst to play with u ", sender
+        );
+    }
+
+    private String sendRequestGameResponse(boolean success) {
+        return createResponse(success, RequestType.SENDER_REQUEST_GAME_ANSWER, "request sent successfully", "failed to send request");
+    }
+
+    private String sendAnswerGameResponse(boolean success) {
+        return createResponse(success, RequestType.RECEIVER_REQUEST_GAME_ANSWER, "answer sent successfully", "failed to send answer");
+    }
+
+    private String createAnswerGameRsponse(boolean success, String name) {
+        return createResponse(success,
+                RequestType.REQUEST_GAME_ANSWER,
+                name + " accepted the request",
+                name + " refused the request"
+        );
+    }
+
+    private void getPlayerIntoGame(String receiver, String sender) throws IOException {
+        new GameRoomHandler(sender, receiver);
+    }
+
 }
